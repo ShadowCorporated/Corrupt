b512eca44a260778806a10d77e297d2e9b0d392b
Bullzee <christopher.bull@uoit.net>
2017-02-14 06:33:16 -0500

    now with sprite animation (collision response fix & enemy detection still needed)

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 #include <fstream>
 #include <streambuf>
-//#include <vector>
+#include <vector>
 #include <iostream>
 #include "Characters.h"
 #include "World Collision.h"
@@ -100,18 +100,32 @@ int main()
 		.set_clear_color(0, 255, 255);
 
 	//create player sprite
-	mainCharacter.load_sprite_image("assets/images/Righteous Thief-1.png")
+	mainCharacter.load_sprite_image("assets/images/RighteousThiefAnim.png")
+		.set_sprite_frame_size(128, 128, false)
 		.set_scale(80, 80)
 		.set_position(player.getX(), player.getY())
 		.set_center(0.5, 0.0); //SET X TO MIDDLE TO ALLOW SPRITE TO FLIP
-		//.set_sprite_frame_size(128, 128);
-		//.push_frame_row("Idle", 0, 0, )
+	for (int r = 0; r < 3; r++)
+		{
+			std::string pid = "anim_" + std::to_string(r);
+			unsigned int row = 128 * r;
+			mainCharacter.push_frame_row(pid , 0, row, 128, 0, 28);
+		}
+	mainCharacter.set_animation("anim_2");
 
 	//create guard sprite
-	guard1.load_sprite_image("assets/images/UNITY Enemy-1.png")
+	guard1.load_sprite_image("assets/images/UNITYEnemyAnim.png")
+		.set_sprite_frame_size(128, 128, false)
 		.set_scale(80, 80)
 		.set_position(enemy1.getX(), enemy1.getY())
 		.set_center(0.5, 0.0); //SET X TO MIDDLE TO ALLOW SPRITE TO FLIP
+
+	for (int r = 0; r < 4; r++)
+	{
+		unsigned int row = 128 * r;
+		guard1.push_frame_row("guardWalk", 0, row, 128, 0, 7);
+	}
+	guard1.set_animation("guardWalk");
 
 	//ground properties
 	ground.setCoord(gx, gy);
@@ -144,7 +158,9 @@ int main()
 		///////////////////////////////////////////
 		//draw sprite(s) per frame//////
 		mainCharacter.draw();
+		mainCharacter.next_frame();
 		guard1.draw();
+		guard1.next_frame();
 		///////////////////////////////
 
 		//player movement info//////
@@ -152,16 +168,21 @@ int main()
 		{
 			velocity.x = -100.0f;
 			mainCharacter.set_scale(-80, 80); //flip sprite
+			mainCharacter.set_animation("anim_1");
 		}
 		else
 			if (right == true) //RIGHT
 			{
 				velocity.x = 100.0f;
 				mainCharacter.set_scale(80, 80); //flip sprite
+				mainCharacter.set_animation("anim_1");
 			}
 
 			else //not moving
+			{
 				velocity.x = 0;
+				mainCharacter.set_animation("anim_2");
+			}
 
 		x += velocity.x * (1.0f / 30.0f); //the player's horizontal velocity updated per frame
 		y += velocity.y * (1.0f / 30.0f); //same as above but this time for vertical velocity
@@ -172,11 +193,13 @@ int main()
 		{
 			enemyVelocity.x = -100.0f; //LEFT
 			guard1.set_scale(-80, 80); //flip sprite
+			//guard1.set_animation("guardWalk");
 		}
 		if (enemy1.getX() <= 900)
 		{
 			enemyVelocity.x = 100.0f; //RIGHT
 			guard1.set_scale(80, 80); //flip sprite
+			//guard1.set_animation("guardWalk");
 		}
 
 		ex += enemyVelocity.x * (1.0f / 30.0f); //the enemy's horizontal velocity updated per frame
@@ -191,13 +214,15 @@ int main()
 		//top of platform
 		if (checkCollide(x, y, 0, 70, platform1.getX(), platform1.getY() + 10, platform1.getWidth(), platform1.getHeight() - 10))
 		{
+			jump = false;
 			velocity.y = 0;
 			gravity.y = 0;
-			jump = false;
 		}
 		else
+		{
+			jump = true;
 			gravity.y = -186.0f;
-
+		}
 		//bottom of platform
 		if (checkCollide(x, y, 0, 70, platform1.getX(), platform1.getY(), platform1.getWidth(), platform1.getHeight() - 5))
 		{
@@ -214,13 +239,19 @@ int main()
 		}
 		else
 		{
-			jump = true;
+			//jump = true; //THIS WAS MAKING IT SO YOU COULDN'T JUMP ON PLATFORM, SO AVOID THIS
 			velocity.y += gravity.y * (1.0f / 30.0f); //this equation effects the y coord of the player
 													  //the velocity of the jump is effected by the gravity and updated per frame
 													  //gives the jump a much smoother effect
 													  //like a PARABOLA :D
 		}
 		////////////////////////
+
+		if (jump == true)
+		{
+			mainCharacter.set_animation("anim_0");
+		}
+		
 		
 		//debugging info
 		//not needed in final version but useful for now
